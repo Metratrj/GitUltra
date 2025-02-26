e6eb5c061627556e79fdc609756ba33144628424
Johannes Grimm <grimmjohannes1998@gmail.com>
2025-02-26 21:47:19 +0100

    adding further boilerplate

--- apps/core/src/lib.rs
+++ apps/core/src/lib.rs
@@ -1,14 +1,35 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use serde::{Deserialize, Serialize};
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let mut builder = Builder::<tauri::Wry>::new().commands(collect_commands![greet,]);
+
+    #[cfg(debug_assertions)]
+    builder
+        .export(Typescript::default(), "../ui/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
+        .invoke_handler(builder.invoke_handler())
+        //.invoke_handler(tauri::generate_handler![greet])
+        .setup(move |app| {
             if cfg!(debug_assertions) {
+                // this is needed to use specta events
+                builder.mount_events(app);
+
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
---
+++ packages/schemas/build.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=flatbuffers/git.fbs");
+
+    let flatc_path = "bin/flatc.exe"; // Path to the local flatc binary
+
+    let status = Command::new(flatc_path)
+        .args(&["--rust", "-o", "src", "flatbuffers/git.fbs"])
+        .status();
+
+    match status {
+        Ok(exit_status) if exit_status.success() => (),
+        _ => panic!("Failed to generate Rust types: `flatc` not found or failed"),
+    }
+}
