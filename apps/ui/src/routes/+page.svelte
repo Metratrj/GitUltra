89626c398432a404ff00f449422693fce810eaac
Johannes Grimm <grimmjohannes1998@gmail.com>
2025-02-26 15:24:25 +0100

    Working - finaly

---
+++ apps/ui/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {invoke} from "@tauri-apps/api/core";
+    let name = $state("");
+    let greetMsg = $state("");
+
+    async function greet(event: Event) {
+        event.preventDefault();
+        greetMsg = await invoke("greet", {name});
+    }
+</script>
+<main class="container">
+    <h1>Welcome to Tauri + Svelte + Specta</h1>
+    <form class="row" onsubmit={greet}>
+        <input id="greet-input" placeholder="Enter a name..." bind:value={name}/>
+        <button type="submit">Greet</button>
+    </form>
+    <p>{greetMsg}</p>
+</main>
