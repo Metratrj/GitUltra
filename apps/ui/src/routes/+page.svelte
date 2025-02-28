0c18e5ee0e1aadceaa921b165d4f70b929644f26
Johannes Grimm <grimmjohannes1998@gmail.com>
2025-02-28 21:16:55 +0100

    Update project configuration and remove unused assets

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-    import {invoke} from "@tauri-apps/api/core";
-    import {Button} from "@/components/ui/button";
-    import {Input} from "@/components/ui/input";
-    import {commands} from "@gitultra/schemas/ts/gitultra/bindings";
+	import { invoke } from "@tauri-apps/api/core";
+	import { Input } from "@/components/ui/input";
+	import { commands } from "@gitultra/schemas/ts/gitultra/bindings";
+	import { Button } from "@/components/ui/button";
 
+	let name = $state("");
+	let greetMsg = $state("");
+	let greet2Msg = $state("");
 
-    let name = $state("");
-    let greetMsg = $state("");
-    let greet2Msg = $state("");
+	async function greet(event: Event) {
+		event.preventDefault();
+		greetMsg = await invoke("greet", { name });
+	}
 
-    async function greet(event: Event) {
-        event.preventDefault();
-        greetMsg = await invoke("greet", {name});
-    }
-
-    async function greet2(event: Event) {
-        event.preventDefault();
-        greet2Msg = await commands.greet(name);
-    }
+	async function greet2(event: Event) {
+		event.preventDefault();
+		greet2Msg = await commands.greet(name);
+	}
 </script>
-<main class="container">
-    <h1>Welcome to Tauri + Svelte + Specta</h1>
-    <form class="row" onsubmit={greet}>
-        <input id="greet-input" placeholder="Enter a name..." bind:value={name}/>
-        <button type="submit">Greet</button>
-    </form>
-    <p>{greetMsg}</p>
-    <br>
-    <form class="row" onsubmit={greet2}>
-        <Input id="greet-input" placeholder="Enter a name..." bind:value={name}/>
-        <Button type="submit">Greet</Button>
-    </form>
-    <p>{greet2Msg}</p>
 
+
+<main class="container">
+	<h1>Welcome to Tauri + Svelte + Specta</h1>
+	<form class="row" onsubmit={greet}>
+		<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<button type="submit">Greet</button>
+	</form>
+	<p>{greetMsg}</p>
+	<br />
+	<form class="row" onsubmit={greet2}>
+		<Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<Button type="submit">Greet</Button>
+	</form>
+	<p>{greet2Msg}</p>
 </main>
